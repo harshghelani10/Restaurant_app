1a66f5b54fb455609f3133a3023392779acad90c
Harsh Ghelani <harshghelani10@gmail.com>
2021-06-05 17:36:07 +0530

    Restaurant_app

--- app/src/main/java/com/example/restaurant_app/model/demo/Demo.java
+++ app/src/main/java/com/example/restaurant_app/model/demo/Demo.java
@@ -180,3 +180,83 @@ public class Demo {
 //    }
 //
 //}
+//package com.example.restaurant_app.model.demo;
+//
+//package com.example.user_view_order;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.example.user_view_order.Retrofit.RetrofitClient;
+//import com.example.user_view_order.Retrofit.RetrofitInterface;
+//
+//import java.util.HashMap;
+//
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//import retrofit2.Response;
+//import retrofit2.Retrofit;
+//
+//public class AllLogin extends AppCompatActivity {
+//
+//    com.example.user_view_order.allLoginmodel.AllLogin allLogin = new com.example.user_view_order.allLoginmodel.AllLogin();
+//    private RetrofitInterface retrofitInterface;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate( savedInstanceState );
+//        setContentView( R.layout.activity_all_login );
+//
+//        EditText email = (EditText) findViewById( R.id.email );
+//        EditText password = (EditText) findViewById( R.id.password );
+//        Button login = (Button) findViewById( R.id.login_btn );
+//
+//
+//        login.setOnClickListener( new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Retrofit retrofitClient = RetrofitClient.getInstance();
+//                retrofitInterface = retrofitClient.create( RetrofitInterface.class);
+//                HashMap<String, String> map = new HashMap<>();
+//
+//                map.put("email", email.getText().toString());
+//                map.put("password", password.getText().toString());
+//
+//                Call<com.example.user_view_order.allLoginmodel.AllLogin> call = retrofitInterface.getaccesslogin(map);
+//
+//                call.enqueue( new Callback<com.example.user_view_order.allLoginmodel.AllLogin>() {
+//                    @Override
+//                    public void onResponse(Call<com.example.user_view_order.allLoginmodel.AllLogin> call, Response<com.example.user_view_order.allLoginmodel.AllLogin> response) {
+//                        if (response.isSuccessful()){
+//                            Toast.makeText( AllLogin.this, "success", Toast.LENGTH_SHORT ).show();
+//                            allLogin = response.body();
+////                            if (allLogin.getRole() == "user"){
+////                                Intent intent = new Intent(getApplicationContext(),UserHome.class);
+////                                startActivity( intent );
+////                            }if(allLogin.getRole() == "admin"){
+////                                Intent i = new Intent(getApplicationContext(),AdminHome.class);
+////                                startActivity( i );
+////                            }
+//
+//                        }else{
+//                            Toast.makeText( AllLogin.this, ""+response.message(), Toast.LENGTH_SHORT ).show();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<com.example.user_view_order.allLoginmodel.AllLogin> call, Throwable t) {
+//                        Toast.makeText( AllLogin.this, ""+t.getLocalizedMessage(), Toast.LENGTH_SHORT ).show();
+//                    }
+//                } );
+//
+//            }
+//        } );
+//
+//    }
+//}
--- app/src/main/java/com/example/restaurant_app/model/demo/Ingredient.java
+++ app/src/main/java/com/example/restaurant_app/model/demo/Ingredient.java
@@ -76,3 +76,58 @@ public class Ingredient {
     }
 
 }
+
+//package com.example.restaurant_app.model.demo;
+//
+//        import com.google.gson.annotations.Expose;
+//        import com.google.gson.annotations.SerializedName;
+//
+//public class Creator {
+//
+//    @SerializedName("name")
+//    @Expose
+//    private String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//}
+
+
+//class MainActivity extends AppCompatActivity {
+//    ImageView logo_image;
+//    TextView welcome_txt;
+//    Button login, sign_up;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView( R.layout.activity_main);
+//
+//        logo_image = (ImageView) findViewById(R.id.logo_image);
+//        welcome_txt = (TextView) findViewById(R.id.welcome_txt);
+//        login = (Button) findViewById(R.id.login);
+//        sign_up = (Button) findViewById(R.id.sign_up);
+//
+//        sign_up.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText( MainActivity.this, "Welcome To Register", Toast.LENGTH_SHORT ).show();
+//
+//            }
+//        });
+//
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText( MainActivity.this, "clicked", Toast.LENGTH_SHORT ).show();
+//
+//            }
+//        });
+//    }
+//}
